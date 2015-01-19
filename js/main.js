012d834ffee70325d7b4d09abf13468d6579e5ed
leehanna <hannallee@berkeley.edu>
2015-01-18 18:09:03 -0800

    responsive

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
-$(document).ready(function(){
-    var $window = $(window);
-    
-    $('section[data-type="background"]').each(function(){
-        var $bgobj = $(this); // assigning the object
-    
-        $(window).scroll(function() {
-            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-            
-            // Put together our final background position
-            var coords = '50% '+ yPos + 'px';
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 
-            // Move the background
-            $bgobj.css({ backgroundPosition: coords });
-        }); 
-    });    
-});
+} else {
+    $(document).ready(function(){
+        var $window = $(window);
+        
+        $('section[data-type="background"]').each(function(){
+            var $bgobj = $(this); // assigning the object
+        
+            $(window).scroll(function() {
+                var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+                
+                // Put together our final background position
+                var coords = '50% '+ yPos + 'px';
+
+                // Move the background
+                $bgobj.css({ backgroundPosition: coords });
+            }); 
+        });    
+    });
+}
 
 function imgView(imagename) {
     var imgPath = "images/rect/";
