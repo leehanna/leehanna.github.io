b8cf0e7ea65944af1c047b37b2adb1aeea7b8891
leehanna <hannallee@berkeley.edu>
2017-02-02 14:51:57 -0800

    thumb overlay jquery fixed

--- js/main.js
+++ js/main.js
@@ -2,15 +2,11 @@ $("#footer").load("footer.html");
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     $(".work").click(function() {
-      // $(this).children('img').eq(0);
-      // window.location = $(this).children('.overlay').eq(0).data("location");
       window.location = $(this).data("location");
-      
-      // window.location = $(".work .overlay").data("location");
       return false;
     });
     $(".thumb").click(function() {
-      window.location = $(".thumb .overlay").data("location");
+      window.location = $(this).data("location");
       return false;
     });
 } else {
