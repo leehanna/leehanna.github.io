b0f3c6ae76fe272c02090e4437367e6a58295d66
leehanna <hannallee@berkeley.edu>
2017-02-02 14:39:33 -0800

    testing grabbing overlay

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ $("#footer").load("footer.html");
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     $(".work").click(function() {
-      window.location = $(".work .overlay").data("location");
+      $(this).children('img').eq(0);
+      window.location = $(this).children('.work .overlay').eq(0).data("location");
+      // window.location = $(".work .overlay").data("location");
       return false;
     });
     $(".thumb").click(function() {
