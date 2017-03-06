0b63249c2731e869570431bfcd79d6f6d7171c50
leehanna <hannallee@berkeley.edu>
2017-03-05 16:56:47 -0800

    added parallax effect to landing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 $("#footer").load("footer.html");
 
+$(document).ready(function(){
+        var $window = $(window);
+        
+        $('section[data-type="background"]').each(function(){
+            var $bgobj = $(this); // assigning the object
+        
+            $(window).scroll(function() {
+                var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+                
+                // Put together our final background position
+                var coords = '50% '+ yPos + 'px';
+
+                // Move the background
+                $bgobj.css({ backgroundPosition: coords });
+            }); 
+        });  
+    });
+
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     $(".work").click(function() {
       window.location = $(this).data("location");
@@ -14,21 +32,6 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
       window.location = $(this).data("location");
       return false;
     });
-    // $(document).ready(function(){
-    //     var $window = $(window);
-        
-    //     $('section[data-type="background"]').each(function(){
-    //         var $bgobj = $(this); // assigning the object
-        
-    //         $(window).scroll(function() {
-    //             var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-                
-    //             // Put together our final background position
-    //             var coords = '50% '+ yPos + 'px';
 
-    //             // Move the background
-    //             $bgobj.css({ backgroundPosition: coords });
-    //         }); 
-    //     });  
-    // });
+    
 }
